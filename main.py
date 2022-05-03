df2b8f004392245a4548ec6018e180641e236b23
mshodge <michael.hodge@ons.gov.uk>
2022-05-03 15:06:36 +0100

    data

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import requests
 import tweepy
 from datetime import datetime
 import os
-
+import csv
 
 post_to_twitter = True
-proxy = False
-github_action = True
+proxy = True
+github_action = False
+
+def update_csv(service, online, timestamp):
+
+    with open('data/data.csv', 'a', newline = "") as f:
+        writer = csv.writer(f)
+        writer.writerow([service, online, timestamp])
+
 
 def post(response, proxy, github_action):
     if github_action:
@@ -39,30 +46,38 @@ def check(proxy):
     url_one_week = "https://www.passportappointment.service.gov.uk/outreach/publicbooking.ofml"
     url_premium = "https://www.passport.service.gov.uk/urgent/?_ga=2.165977918.1052226504.1651564347-663154096.1628163070"
 
+    headers = requests.utils.default_headers()
+    headers.update({
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
+    })
+
     if proxy:
         import config.proxies as set_proxies
-        headers = requests.utils.default_headers()
-        headers.update({
-            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
-        })
 
         proxies = set_proxies.set_ons_proxies(ssl = False, headers = headers)
         page_1_week = requests.get(url_one_week, proxies = proxies, verify = False, headers = headers)
         page_premium = requests.get(url_premium, proxies = proxies, verify = False, headers = headers)
 
     else:
-        page_1_week = requests.get(url_one_week)
-        page_premium = requests.get(url_premium)
+        page_1_week = requests.get(url_one_week, verify = False, headers = headers)
+        page_premium = requests.get(url_premium, verify = False, headers = headers)
 
-    if "Sorry, this service is unavailable" in page_1_week.text:
-        response = f"One week fast track service is unavailable ❎ ({datetime.now().strftime('%d/%m/%Y %H:%M')}) https://www.gov.uk/get-a-passport-urgently/1-week-fast-track-service"
-    else:
-        response = f"One week fast track service is available ✅ ({datetime.now().strftime('%d/%m/%Y %H:%M')}) https://www.gov.uk/get-a-passport-urgently/1-week-fast-track-service"
+    timestamp = datetime.now().strftime('%d/%m/%Y %H:%M')
 
-    if "Sorry, this service is unavailable" in page_premium.text:
-        response += f"\nPremium service is unavailable ❎ ({datetime.now().strftime('%d/%m/%Y %H:%M')}) https://www.gov.uk/get-a-passport-urgently/online-premium-service"
+
+    if "service is unavailable" in page_1_week.text:
+        response = f"One week fast track service is unavailable ❌ ({timestamp}) https://www.gov.uk/get-a-passport-urgently/1-week-fast-track-service"
+        update_csv(service = "one week fast track", online = "False", timestamp = timestamp)
     else:
-        response += f"\nPremium service is available ✅ ({datetime.now().strftime('%d/%m/%Y %H:%M')}) https://www.gov.uk/get-a-passport-urgently/online-premium-service"
+        response = f"One week fast track service is available ✅ ({timestamp}) https://www.gov.uk/get-a-passport-urgently/1-week-fast-track-service"
+        update_csv(service = "one week fast track", online = "True", timestamp = timestamp)
+
+    if "service is unavailable" in page_premium.text:
+        response += f"\nPremium service is unavailable ❌ ({timestamp}) https://www.gov.uk/get-a-passport-urgently/online-premium-service"
+        update_csv(service = "premium", online = "False", timestamp = timestamp)
+    else:
+        response += f"\nPremium service is available ✅ ({timestamp}) https://www.gov.uk/get-a-passport-urgently/online-premium-service"
+        update_csv(service = "premium", online = "True", timestamp = timestamp)
 
     print(response)
     return response
