d8982aed4e2aad7d1c5f952e8e4ab2a8a6d5119e
Michael Hodge <michael.hodge@ons.gov.uk>
2022-05-06 20:38:12 +0100

    vars change

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ import urllib3
 urllib3.disable_warnings()
 
 proxy = False
-github_action = False
-save_csv = False
-post_to_twitter = False
+github_action = True
+save_csv = True
+post_to_twitter = True
 
 def df_to_csv_string(df):
     """
