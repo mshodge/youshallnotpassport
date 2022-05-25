dbc8a4ed40e9cf37388715c4fc4c984bfbca87bb
Michael Hodge <michael.hodge@ons.gov.uk>
2022-05-25 15:17:25 +0100

    refactor

--- main_dev.py
+++ main_dev.py
@@ -13,13 +13,33 @@ is_github_action = True
 to_save_csv = False
 is_twitter = True
 
-def update_twitter_bio(github_action, proxy, one_week_status, premium_status):
+
+# //TODO: Add in auth Twitter function
+
+def get_timestamp(github_action, timestamp_string_format='%H:%M'):
+    """
+    Gets timestamp
+    :param github_action: <Boolean> Whether a GitHub action or not, for auth
+    :param timestamp_string_format: <string> The datetime format to return
+    :return: timestamp <string> The formatted datetime
+    """
     if github_action:
         timestamp = datetime.now() + timedelta(hours=1)
-        timestamp = timestamp.strftime('%H:%M')
-
+        timestamp = timestamp.strftime(timestamp_string_format)
     else:
-        timestamp = datetime.now().strftime('%H:%M')
+        timestamp = datetime.now().strftime(timestamp_string_format)
+    return timestamp
+
+
+def update_twitter_bio(github_action, proxy, one_week_status, premium_status):
+    """
+    Update Twitter Bio
+    :param github_action: <Boolean> Whether a GitHub action or not, for auth
+    :param proxy: <Boolean> Whether using a proxy or not
+    :param one_week_status: <string> The status of the one week service
+    :param premium_status: <string> The status of the premium service
+    """
+    timestamp = get_timestamp(github_action, timestamp_string_format='%H:%M')
 
     # Uses GitHub Secrets to store and load credentials
     if github_action:
@@ -45,52 +65,54 @@ def update_twitter_bio(github_action, proxy, one_week_status, premium_status):
     else:
         api = tweepy.API(auth, wait_on_rate_limit=True)
 
-    if premium_status == "False":
-        premium_status_symbol = f"OP is offline,"
-    elif premium_status == "Busy":
+    if premium_status == "Busy":
         premium_status_symbol = f"OP ️is busy,"
     elif premium_status == "True":
         premium_status_symbol = f"OP is online,"
+    else:
+        premium_status_symbol = f"OP is offline,"
 
-    if one_week_status == "False":
-        one_week_status_symbol = f"FT is offline"
-    elif one_week_status == "Busy":
+    if one_week_status == "Busy":
         one_week_status_symbol = f"FT is busy"
     elif one_week_status == "True":
         one_week_status_symbol = f"FT is online"
+    else:
+        one_week_status_symbol = f"FT is offline"
 
     new_bio = f"Unofficial bot. Runs every ~30 mins. Please check http://gov.uk/get-a-passport-urgently before " \
               f"booking. {premium_status_symbol} {one_week_status_symbol} (updated {timestamp})."
 
     # Posts status to Twitter
-    api.update_profile(description = new_bio)
+    api.update_profile(description=new_bio)
 
     print("Updated bio on Twitter")
+
 
 def read_online_status():
     """
     Loads online status file
-    :return: <DataFrame> The Dataframe with online status in
+    :return: df_online_status <DataFrame> The Dataframe with online status in
     """
 
-    df = pd.read_csv("https://raw.githubusercontent.com/mshodge/youshallnotpassport/main/data/online.csv")
-    return df
+    df_online_status = pd.read_csv("https://raw.githubusercontent.com/mshodge/youshallnotpassport/main/data/online.csv")
+    return df_online_status
 
-def online_status_on_last_check(df, service):
+
+def online_status_on_last_check(df_old_online_status, service):
     """
     Returns value from dataframe
-    :param df: <DataFrame> The DataFrame of online status
+    :param df_old_online_status: <DataFrame> The DataFrame of online status
     :param service: <string> The service type
     :return: <string> Whether the status is online ('True', 'Busy') or not ('False')
     """
 
-    return str(df[df['service'] == service]['online'].bool())
+    return str(df_old_online_status[df_old_online_status['service'] == service]['online'].bool())
 
 
-def update_online_status(df, github_action):
+def update_online_status(df_status, github_action):
     """
     Updates csv file on GitHub and local
-    :param df_response_from_check: <DataFrame> The DataFrame from the current check
+    :param df_status: <DataFrame> The DataFrame from the current check
     :param github_action: <Boolean> Whether a GitHub action or not, for auth
     :return: <string> The response of whether the service is online or not
     """
@@ -103,7 +125,7 @@ def update_online_status(df, github_action):
     branch = "main"
     file_path = "data/online.csv"
 
-    df_string = df_to_csv_string(df)
+    df_string = df_to_csv_string(df_status)
 
     if github_action:
         token = os.environ['access_token_github']
@@ -126,26 +148,26 @@ def update_online_status(df, github_action):
     df.to_csv(file_path)
 
 
-def df_to_csv_string(df):
+def df_to_csv_string(df_to_convert):
     """
     Converts a pandas DataFrame into a csv string for upload to GitHub
-    :param df: <DataFrame> The DataFrame
+    :param df_to_convert: <DataFrame> The DataFrame
     :return: df_string <string> The rDa
     """
 
     df_string = ""
 
     # For columns
-    for idx, value in enumerate(df.columns.to_list()):
-        if idx < df.columns.shape[0] - 1:
+    for idx, value in enumerate(df_to_convert.columns.to_list()):
+        if idx < df_to_convert.columns.shape[0] - 1:
             df_string += value + ","
         else:
             df_string += value + "\n"
 
     # For all rows in DataFrame
-    for index, row in df.iterrows():
+    for index, row in df_to_convert.iterrows():
         for idx, value in enumerate(row.to_list()):
-            if idx < df.columns.shape[0] - 1:
+            if idx < df_to_convert.columns.shape[0] - 1:
                 df_string += str(value) + ","
             else:
                 df_string += str(value) + "\n"
@@ -170,9 +192,9 @@ def update_csv(df_response_from_check, github_action):
     file_path = "data/data.csv"
     csv_url = f'https://raw.githubusercontent.com/{org}/{repo}/{branch}/{file_path}'
 
-    df = pd.read_csv(csv_url)
-    df = pd.concat([df, df_response_from_check], ignore_index=True)
-    df_string = df_to_csv_string(df)
+    df_data = pd.read_csv(csv_url)
+    df_data = pd.concat([df_data, df_response_from_check], ignore_index=True)
+    df_string = df_to_csv_string(df_data)
 
     if github_action:
         token = os.environ['access_token_github']
@@ -274,71 +296,66 @@ def check(proxy, github_action):
 
     # GitHub uses GMT and not BST so adjusting for that here
     # //TODO: Make this more dynamic and not hard coded, as when BST ends this will trip up
-    if github_action:
-        timestamp = datetime.now() + timedelta(hours=1)
-        timestamp = timestamp.strftime('%d/%m/%Y %H:%M')
-
-    else:
-        timestamp = datetime.now().strftime('%d/%m/%Y %H:%M')
+    timestamp = get_timestamp(github_action, timestamp_string_format='%d/%m/%Y %H:%M')
 
     # Reports if one week service is online or not
     if "service is unavailable" in page_one_text:
         response_one_week = f"One week fast track service is now offline ❌ ({timestamp})" \
-                    f"\n" \
-                    f"\n" \
-                    f"Bot checks every 5 mins, and will post again if status changes."\
-                    f"\n" \
-                    f"https://www.gov.uk/get-a-passport-urgently/1-week-fast-track-service"
+                            f"\n" \
+                            f"\n" \
+                            f"Bot checks every 5 mins, and will post again if status changes." \
+                            f"\n" \
+                            f"https://www.gov.uk/get-a-passport-urgently/1-week-fast-track-service"
         one_week_online = "False"
     elif "System busy" in page_one_text:
         response_one_week = f"One week fast track service is still online but busy ⚠️ ({timestamp})" \
-                    f"\n" \
-                    f"\n" \
-                    f"Bot checks every 5 mins, and will post again if status changes."\
-                    f"\n" \
-                    f"https://www.gov.uk/get-a-passport-urgently/1-week-fast-track-service"
+                            f"\n" \
+                            f"\n" \
+                            f"Bot checks every 5 mins, and will post again if status changes." \
+                            f"\n" \
+                            f"https://www.gov.uk/get-a-passport-urgently/1-week-fast-track-service"
         one_week_online = "Busy"
     else:
         response_one_week = f"One week fast track service is now online! ✅ ({timestamp})" \
-                    f"\n" \
-                    f"\n" \
-                    f"Bot checks every 5 mins, and will post again if status changes."\
-                    f"\n" \
-                    f"https://www.gov.uk/get-a-passport-urgently/1-week-fast-track-service"
+                            f"\n" \
+                            f"\n" \
+                            f"Bot checks every 5 mins, and will post again if status changes." \
+                            f"\n" \
+                            f"https://www.gov.uk/get-a-passport-urgently/1-week-fast-track-service"
         one_week_online = "True"
 
     # Reports if premium service is online or not
     if "service is unavailable" in page_premium_text:
         response_premium = f"Premium service is now offline ❌ ({timestamp})" \
-                    f"\n" \
-                    f"\n" \
-                    f"Bot checks every 5 mins, and will post again if status changes."\
-                    f"\n" \
-                    f"https://www.gov.uk/get-a-passport-urgently/online-premium-service"
+                           f"\n" \
+                           f"\n" \
+                           f"Bot checks every 5 mins, and will post again if status changes." \
+                           f"\n" \
+                           f"https://www.gov.uk/get-a-passport-urgently/online-premium-service"
         premium_online = "False"
     elif "Sorry, there are no available appointments" in page_premium_text:
         response_premium = f"Premium service now offline ❌ ({timestamp})" \
-                    f"\n" \
-                    f"\n" \
-                    f"Bot checks every 5 mins, and will post again if status changes"\
-                    f"\n" \
-                    f"https://www.gov.uk/get-a-passport-urgently/online-premium-service"
+                           f"\n" \
+                           f"\n" \
+                           f"Bot checks every 5 mins, and will post again if status changes" \
+                           f"\n" \
+                           f"https://www.gov.uk/get-a-passport-urgently/online-premium-service"
         premium_online = "False"
     elif "System busy" in page_premium_text:
         response_premium = f"Premium service is still online but busy ⚠️ ({timestamp})" \
-                    f"\n" \
-                    f"\n" \
-                    f"Bot checks every 5 mins, and will post again if status changes."\
-                    f"\n" \
-                    f"https://www.gov.uk/get-a-passport-urgently/online-premium-service"
+                           f"\n" \
+                           f"\n" \
+                           f"Bot checks every 5 mins, and will post again if status changes." \
+                           f"\n" \
+                           f"https://www.gov.uk/get-a-passport-urgently/online-premium-service"
         premium_online = "Busy"
     else:
         response_premium = f"Premium service is now online! ✅ ({timestamp})" \
-                    f"\n" \
-                    f"\n" \
-                    f"Bot checks every 5 mins, and will post again if status changes."\
-                    f"\n" \
-                    f"https://www.gov.uk/get-a-passport-urgently/online-premium-service"
+                           f"\n" \
+                           f"\n" \
+                           f"Bot checks every 5 mins, and will post again if status changes." \
+                           f"\n" \
+                           f"https://www.gov.uk/get-a-passport-urgently/online-premium-service"
         premium_online = "True"
 
     print(response_one_week)
