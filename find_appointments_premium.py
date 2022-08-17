cc4fb260be1eb35b0fb351c3556debd8943dad30
Michael Hodge <michael.hodge@ons.gov.uk>
2022-08-17 10:00:45 +0100

    increase to more than 1

--- find_appointments_premium.py
+++ find_appointments_premium.py
@@ -30,7 +30,7 @@ def check_diff_in_loc_counts(df):
     df_diff['count'] = df['count'] - df_old['count']
     locs_added = []
     for index, row in df_diff.iterrows():
-        if row['count'] > 0:
+        if row['count'] > 1:
             locs_added.append(row['location'])
 
     return locs_added
