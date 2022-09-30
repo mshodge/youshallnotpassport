dad8b33294199f613e9dbbf9c5ebc9735da7f80a
Michael Hodge <michael.hodge@ons.gov.uk>
2022-09-30 14:38:34 +0100

    sub

--- scripts/utils/sms.py
+++ scripts/utils/sms.py
@@ -11,17 +11,23 @@ def call_sms(service, type, response):
     if service == "Fast Track":
         if type == "status":
             if "✅" in response:
-                message = f"Fast Track service is now online https://www.gov.uk/get-a-passport-urgently/1-week-fast-track-service."
+                message = f"Fast Track service is now online " \
+                          f"https://www.gov.uk/get-a-passport-urgently/1-week-fast-track-service. You can cancel" \
+                          f"your subscription here https://billing.stripe.com/p/login/6oE9Dv4Nw89G2QMaEE"
             else:
-                message = f"Fast Track service is now offline."
+                message = f"Fast Track service is now offline. You can cancel" \
+                          f"your subscription here https://billing.stripe.com/p/login/6oE9Dv4Nw89G2QMaEE"
         if type == "app":
             message = response
     elif service == "Premium":
         if type == "status":
             if "✅" in response:
-                message = f"Premium service is now online https://www.gov.uk/get-a-passport-urgently/online-premium-service."
+                message = f"Premium service is now online " \
+                          f"https://www.gov.uk/get-a-passport-urgently/online-premium-service. You can cancel" \
+                          f"your subscription here https://billing.stripe.com/p/login/6oE9Dv4Nw89G2QMaEE"
             else:
-                message = f"Premium service is now offline."
+                message = f"Premium service is now offline. You can cancel" \
+                          f"your subscription here https://billing.stripe.com/p/login/6oE9Dv4Nw89G2QMaEE"
         if type == "app":
             message = response
 
