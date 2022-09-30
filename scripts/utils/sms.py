0741aa88b8168d3950e06c88b4bb1c7a5eedc2c2
Michael Hodge <michael.hodge@ons.gov.uk>
2022-09-30 12:46:39 +0100

    feat

--- scripts/utils/sms.py
+++ scripts/utils/sms.py
@@ -10,13 +10,13 @@ def call_sms(service, type, response):
 
     if service == "Fast Track":
         if type == "status":
-            if ✅ in response:
+            if "✅" in response:
                 message = f"Fast Track service is now online https://www.gov.uk/get-a-passport-urgently/1-week-fast-track-service."
             else:
                 message = f"Fast Track service is now offline."
     elif service == "Premium":
         if type == "status":
-            if ✅ in response:
+            if "✅" in response:
                 message = f"Premium service is now online https://www.gov.uk/get-a-passport-urgently/online-premium-service."
             else:
                 message = f"Premium service is now offline."
