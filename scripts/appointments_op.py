406030c48acb09272c59f10fa929965857aab1b4
mshodge <michael.hodge@digital.justice.gov.uk>
2023-02-17 10:33:33 +0000

    test getting all appointments

--- scripts/appointments_op.py
+++ scripts/appointments_op.py
@@ -16,10 +16,18 @@ import pandas as pd
 from IPython.display import display
 from tabulate import tabulate
 
+# For local testing - to delete
+# import chromedriver_autoinstaller
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.options import Options
+# from selenium.common.exceptions import NoSuchElementException
+# chromedriver_autoinstaller.install()
+
 __author__ = ['Dr. Usman Kayani']
 
 MAIN_URL = 'https://www.passport.service.gov.uk/urgent/'
-MAIN_HEADERS = {    
+MAIN_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.5',
@@ -27,6 +35,7 @@ MAIN_HEADERS = {
 }
 
 session = requests.Session()
+
 
 def form_data(data: dict) -> str:
     """Form the data for a POST request.
@@ -36,6 +45,7 @@ def form_data(data: dict) -> str:
             The data to form.
     """
     return '&'.join(['{}={}'.format(k, v) for k, v in data.items()])
+
 
 def get_token(data: str) -> str:
     """Get the token from the data.
@@ -56,6 +66,7 @@ def get_token(data: str) -> str:
     else:
         return None
 
+
 def get_appointment_data(MAIN_URL) -> pd.DataFrame:
     """Get the appointment data.
 
@@ -67,35 +78,54 @@ def get_appointment_data(MAIN_URL) -> pd.DataFrame:
         pd.DataFrame
             The dataframe with the appointment data.
     """
-    session.headers = MAIN_HEADERS
+    # session.headers = MAIN_HEADERS
     form_datas = [
-        {'is-uk-application' : 'true'},
+        {'is-uk-application': 'true'},
         {
-            'date-of-birth-day' : '01',
-            'date-of-birth-month' : '01',
-            'date-of-birth-year' : '1990',
+            'date-of-birth-day': '01',
+            'date-of-birth-month': '01',
+            'date-of-birth-year': '1990',
         },
-        {'previous-passport' : 'true'},
+        {'previous-passport': 'true'},
         {'passport-lost': 'false'},
-        {'name-changed' : 'false'},
+        {'name-changed': 'false'},
         {
-            'passport-issue-day' : '01',
-            'passport-issue-month' : '01',
-            'passport-issue-year' : '2010',
+            'passport-issue-day': '01',
+            'passport-issue-month': '01',
+            'passport-issue-year': '2010',
         },
-        {'damaged' : 'false'},
-        {'other-passports' : 'false'},
+        {'damaged': 'false'},
+        {'other-passports': 'false'},
         {},
         {},
     ]
 
     start_urls = [
-        MAIN_URL, 
+        MAIN_URL,
         'https://www.passport.service.gov.uk/filter/start/urgent'
     ]
 
+    # For local testing - to delete
+    # options = Options()
+    # options.add_argument('--disable-gpu')
+    # options.add_argument('--no-sandbox')
+    # options.add_argument('--window-size=1920,1080')
+    # the_driver = webdriver.Chrome(options=options)
+
     for url in start_urls:
         data = session.get(url)
+        # the_driver.get(url) # For local testing - to delete
+
+    # For local testing - to delete
+    # session = requests.Session()
+    # # Set correct user agent
+    # selenium_user_agent = the_driver.execute_script("return navigator.userAgent;")
+    # session.headers.update({"user-agent": selenium_user_agent})
+    #
+    # for cookie in the_driver.get_cookies():
+    #     session.cookies.set(cookie['name'], cookie['value'], domain=cookie['domain'])
+    #
+    # data = session.get(url)
 
     no_appt_text = 'Sorry, there are no available appointments'
     if no_appt_text in data.text:
@@ -109,12 +139,15 @@ def get_appointment_data(MAIN_URL) -> pd.DataFrame:
         'Content-Type': 'application/x-www-form-urlencoded',
         'Origin': 'https://www.passport.service.gov.uk'
     }
+
+    list_of_urls = []
+
     session.headers.update(form_headers)
 
     for form in form_datas:
         data = session.post(
-            curr_url, 
-            data=form_data({**form, 'x-csrf-token': csrf_token}), 
+            curr_url,
+            data=form_data({**form, 'x-csrf-token': csrf_token}),
             allow_redirects=True
         )
         curr_url = data.url
@@ -128,8 +161,11 @@ def get_appointment_data(MAIN_URL) -> pd.DataFrame:
 
     df = pd.read_html(data.text)[0]
     first_date = datetime.strptime(df.columns[0], '%A  %d %B').replace(year=curr_year) + timedelta(days=5)
+
+    list_of_urls = update_list_of_urls(data, list_of_urls)
+
     start_dates = [
-        (first_date + timedelta(days=6*i)).strftime(f'%Y-%m-%d')
+        (first_date + timedelta(days=6 * i)).strftime(f'%Y-%m-%d')
         for i in range(4)
     ]
 
@@ -138,13 +174,51 @@ def get_appointment_data(MAIN_URL) -> pd.DataFrame:
     for date in start_dates:
         first_page = f'https://www.passport.service.gov.uk/booking/choose-date-and-place/{date}/next'
         data = session.get(first_page)
+        list_of_urls = update_list_of_urls(data, list_of_urls)
         try:
             curr_df = pd.read_html(data.text)[0]
             data_list.append(curr_df)
         except:
             pass
 
-    return clean_df(pd.concat(data_list, axis=1))
+    cleaned_df = clean_df(pd.concat(data_list, axis=1))
+    print(f"Checking actual number of appointments.")
+
+    for url in list_of_urls:
+        data_table_data = session.get("https://www.passport.service.gov.uk" + url)
+        df_table = pd.read_html(data_table_data.text)[0]
+        date_is = datetime.strptime(url.split("/")[-1], '%Y-%m-%d').strftime('%a %-e %b')
+        location_is = url.split("/")[-2].capitalize()
+        count_is = df_table.iloc[:, 0].to_string().count("Available")
+        cleaned_df.at[location_is, date_is] = count_is
+
+    return cleaned_df
+
+
+def update_list_of_urls(session_data, list_of_urls_to_update) -> list:
+    """
+    Gathers a list of urls to get the appointments data from
+
+    Args:
+        session_data:
+            requests session data
+        list_of_urls_to_update: list
+            A list of already gathered urls
+
+    Returns:
+        list_of_urls_to_update: list
+            The updated list
+    """
+    soup = BeautifulSoup(session_data.text, 'html.parser')
+    table = soup.find('table', {'class': "govuk-table booking-table"})
+
+    if table is not None:
+        for a in table.findAll('a'):
+            url = a.get("href")
+            list_of_urls_to_update.append(url)
+
+    return list_of_urls_to_update
+
 
 def clean_df(df) -> pd.DataFrame:
     """
@@ -162,12 +236,12 @@ def clean_df(df) -> pd.DataFrame:
         df.loc[len(df)] = "Birmingham  Unavailable"
 
     locations = [
-        "London", 
-        "Peterborough", 
-        "Newport", 
-        "Liverpool", 
-        "Durham", 
-        "Glasgow", 
+        "London",
+        "Peterborough",
+        "Newport",
+        "Liverpool",
+        "Durham",
+        "Glasgow",
         "Belfast",
         "Birmingham"
     ]
@@ -207,6 +281,7 @@ def clean_df(df) -> pd.DataFrame:
     nice_df = nice_df.astype(float)
 
     return nice_df
+
 
 if __name__ == '__main__':
     data = get_appointment_data(MAIN_URL)
